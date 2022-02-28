274c51f5e4cdeefc5143f86532115ac5bbef69a0
Kelvin Nguyen <k.nguyen.pwc@gmail.com>
2022-02-28 17:26:50 +1100

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Belly-Button-Biodiversity-Plotly
+# Plot.ly - Belly Button Biodiversity
+
+In this assignment, I built an interactive dashboard to explore the [Belly Button Biodiversity dataset](http://robdunnlab.com/projects/belly-button-biodiversity/), which catalogs the microbes that colonise human navels.
+
+The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+  1. Use the D3 library to read the data
+
+  2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+
+  3. Create a bubble chart that displays each sample. `otu_ids` was used for the marker colours (effecting the red and blue main color).
+
+  4. Display the sample metadata, i.e., an individual's demographic information.
+
+  5. Gauge chart to plot the weekly washing frequency of the individual.
+
+  6. Display each key-value pair from the metadata JSON object somewhere on the page.
+
+  7. Update all of the plots any time that a new sample is selected.
+
+All charts are responsive to window sizes
+
+##  About the Data
+
+Hulcr, J. et al.(2012) _A Jungle in There: Bacteria in Belly Buttons are Highly Diverse, but Predictable_. Retrieved from: [http://robdunnlab.com/projects/belly-button-biodiversity/results-and-data/](http://robdunnlab.com/projects/belly-button-biodiversity/results-and-data/)
+
+The dataset is stored in [samples.json](data/samples.json)
---
+++ static/js/app.js
@@ -0,0 +1,234 @@
+// *********************************************
+// *********** PAGE INITIALIZATION *************
+// *********************************************
+
+d3.json("data/samples.json").then(function(data){
+    // populate test subject ID and allocate to the drop down selection
+    d3.select("#selDataset").selectAll("option")
+        .data(data.names)
+        .enter()
+        .append("option")
+        .text( d=> d)
+        .attr("value", d => d)
+    
+    // set default test ID to the first item on the name list
+    var selectedValue = data.names[0];
+    
+    // starting up web page with default selection
+    handleSelection(selectedValue);
+});
+
+
+// *********************************************
+// ********** CREATE VISUALIZATIONS ************
+// *********************************************
+
+function handleSelection(id){
+    // data extraction
+    d3.json("data/samples.json").then(function(data){
+        console.log("Dataset:",data);
+        // filter data based on selected ID
+        var filteredData = data.samples.filter(sample => sample.id == id)[0]
+        
+
+        // **************************************
+        // ********* UPDATE BAR CHART ***********
+        // **************************************
+
+        // 1. getting data for bar chart
+        // 2. use only 10 records
+        // 3. sort data ascending based on sample values, since the data in sample value was already sorted, we use reverse function
+        var sample_values = filteredData.sample_values.slice(0,10).reverse();
+        var otu_ids = filteredData.otu_ids.slice(0,10).reverse();
+        var otu_labels = filteredData.otu_labels.slice(0,10).reverse();
+        
+        trace = {
+            x : sample_values,
+            y : otu_ids.map(id => `OTU ${id}`),
+            text: otu_labels,
+            orientation: "h",
+            type: "bar",
+        };
+        layout = {
+            autosize:true,
+            title:`<b>Top 10 operational axonomic units (OTUs)<b>`,
+            xaxis:{fontsize: 1, title: "sample value"}
+        }
+        Plotly.newPlot("bar", [trace], layout, {responsive: true});
+
+
+        // **************************************
+        // ******** UPDATE BULLET CHART *********
+        // **************************************
+
+        // to see how much top OTUS from the total sample value
+        var totalTop10 = sample_values.reduce((a, b) => a + b, 0);
+        var totalSampleValue = filteredData.sample_values.reduce((a, b) => a + b, 0);
+
+        var bulletData = [{
+              type: "indicator",
+              mode: "number+gauge",
+              value: totalTop10,
+              title: { text: `<b>${Math.floor(totalTop10/totalSampleValue*100)}%</b>` },
+              gauge: {
+                shape: "bullet",
+                axis: { range: [null, totalSampleValue] }
+              }
+            }];
+          
+          var layout = {title:`<b>Total sample value - Top 10 OTUs vs ALL</b>`,
+                        height: 250
+                    };
+          
+          Plotly.newPlot('bullet', bulletData, layout, {responsive: true});
+        
+
+        // ************************************** 
+        // ******** UPDATE BUBBLE CHART *********
+        // **************************************
+
+        sample_values = filteredData.sample_values;
+        otu_ids = filteredData.otu_ids;
+        otu_labels = filteredData.otu_labels;
+        
+        console.log("IDs:",Math.max(...otu_ids));
+        var trace1 = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: 'markers',
+            marker: {
+              color: otu_ids.map(d => `rgb(${Math.floor(d/Math.max(...otu_ids)*255)},
+                                            100,
+                                            ${255-Math.floor(d/Math.max(...otu_ids)*255)})`),
+              size: sample_values
+            }
+        };
+          
+          var layout = {
+                title: `<b> Bubble chart - OTUs' samples </b>`,
+                autosize:true,
+                showlegend: false,
+                xaxis: {title: "OTU ID"},
+                yaxis: {title: "sample value",
+                        orientation:"h"},
+          };
+          
+          Plotly.newPlot('bubble', [trace1], layout, {responsive: true});
+           
+
+        // **************************************
+        // ****** UPDATE DEMOGRAPHIC INFO *******
+        // **************************************
+                
+        var testProfile = data.metadata.filter(profile => profile.id == id)[0]; 
+
+        // loop through the matching ID profile to display the demographic info
+        var arr = Object.entries(testProfile).map(([key, value]) => `${key}: ${value}`);
+        
+        // update data
+        var demoSection = d3.select("#sample-metadata")
+                            .selectAll("h6")
+                            .data(arr)
+        
+            // enter data
+            demoSection
+                .enter()
+                .append("h6")
+                .merge(demoSection)
+                .text(d=>d)
+
+            // remove extra element if the updated profile is short of information
+            demoSection.exit().remove()
+            
+
+        // **************************************    
+        // *******   UPDATE GAUGE CHART   *******
+        // **************************************
+        
+        var washFreq = testProfile.wfreq;
+            
+        // create a function to draw needle base on washFreq value
+        function gaugePointer(wfreq){
+                // each wash frequency takes 20 degrees
+            var degrees = 180 - wfreq*20,
+                radius = .65;
+            var radians = degrees * Math.PI / 180;
+            var x = radius * Math.cos(radians);
+            var y = radius * Math.sin(radians);
+
+            // create path
+            var mainPath = 'M -.0 -0.035 L .0 0.035 L ',
+                pathX = String(x),
+                space = ' ',
+                pathY = String(y),
+                pathEnd = ' Z';
+            var path = mainPath.concat(pathX,space,pathY,pathEnd);
+                
+                return path;
+        }
+
+        // data for drawing needle button
+        var buttonData = { type: 'scatter',
+                            x:[0],
+                            y:[0],
+                            marker: {size: 15, color:'850000'},
+                            showlegend: false,
+                            name: 'Wash frequency',
+                            text: washFreq,
+                            hoverinfo: 'text+name'
+                        }
+                        
+        var pieData = { values: [1,1,1,1,1,1,1,1,1,9],
+                            rotation: 90,
+                            text: ['8-9', '7-8', '6-7','5-6', '4-5', '3-4', '2-3', '1-2', '0-1'],
+                            textinfo: 'text',
+                            textposition:'inside',	  
+                            marker: {colors:['rgba(14, 127, 0, .5)',
+                                            'rgba(34, 130, 12, .5)',
+                                            'rgba(54, 149, 24, .5)',
+                                            'rgba(110, 154, 36, .5)',
+                                            'rgba(170, 170, 48, .5)',
+                                            'rgba(190, 190, 60, .5)', 
+                                            'rgba(202, 202, 72, .5)',
+                                            'rgba(210, 213, 84, .5)', 
+                                            'rgba(232, 226, 96, .5)',
+                                            'rgba(255, 255, 255, 0)']},
+                            hoverinfo: 'text',
+                            hole: 0.4,
+                            type: 'pie',
+                            showlegend: false
+                        };
+        var data = [buttonData,pieData];
+
+        var layout = {
+            autosize:true,
+            shapes:[{
+                type: 'path',
+                path: gaugePointer(washFreq),
+                fillcolor: '850000',
+                line: {
+                    color: '850000'
+                }
+                }],
+            title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per week',
+            autosize:true,
+            xaxis: {
+                zeroline:false, 
+                showticklabels:false,
+                showgrid: false, 
+                range: [-1, 1]
+            },
+            yaxis: {
+                zeroline:false, 
+                showticklabels:false,
+                showgrid: false, 
+                range: [-1, 1]
+            }
+        };
+
+        Plotly.newPlot('gauge', data, layout, {responsive: true});
+    });
+};
+
+
