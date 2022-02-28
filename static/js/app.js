065bc6eda246466e1915e7fbf8dfd150969e3b4d
Kelvin Nguyen <k.nguyen.pwc@gmail.com>
2022-02-28 23:18:47 +1100

    cleaned script

--- static/js/app.js
+++ static/js/app.js
@@ -61,15 +61,18 @@ function handleSelection(id){
         // ******** UPDATE BULLET CHART *********
         // **************************************
 
-        // to see how much top OTUS from the total sample value
+        // to see how much top OTUS took from the total sample value
         var totalTop10 = sample_values.reduce((a, b) => a + b, 0);
         var totalSampleValue = filteredData.sample_values.reduce((a, b) => a + b, 0);
+
+        // show percentage on bullet chart
+        var percentageTop10 = `<b>${Math.floor(totalTop10/totalSampleValue*100)}%</b>`
 
         var bulletData = [{
               type: "indicator",
               mode: "number+gauge",
               value: totalTop10,
-              title: { text: `<b>${Math.floor(totalTop10/totalSampleValue*100)}%</b>` },
+              title: { text: percentageTop10},
               gauge: {
                 shape: "bullet",
                 axis: { range: [null, totalSampleValue] }
@@ -86,18 +89,19 @@ function handleSelection(id){
         // ************************************** 
         // ******** UPDATE BUBBLE CHART *********
         // **************************************
-
+        
+        // extract data
         sample_values = filteredData.sample_values;
         otu_ids = filteredData.otu_ids;
         otu_labels = filteredData.otu_labels;
         
-        console.log("IDs:",Math.max(...otu_ids));
         var trace1 = {
             x: otu_ids,
             y: sample_values,
             text: otu_labels,
             mode: 'markers',
             marker: {
+              // set red and blue color coresponding to the OTU id
               color: otu_ids.map(d => `rgb(${Math.floor(d/Math.max(...otu_ids)*255)},
                                             100,
                                             ${255-Math.floor(d/Math.max(...otu_ids)*255)})`),
@@ -149,7 +153,7 @@ function handleSelection(id){
         var washFreq = testProfile.wfreq;
             
         // create a function to draw needle base on washFreq value
-        function gaugePointer(wfreq){
+        function gaugeNeedle(wfreq){
                 // each wash frequency takes 20 degrees
             var degrees = 180 - wfreq*20,
                 radius = .65;
@@ -168,7 +172,7 @@ function handleSelection(id){
                 return path;
         }
 
-        // data for drawing needle button
+        // data for drawing needle button, just one point shown on a scatter plot
         var buttonData = { type: 'scatter',
                             x:[0],
                             y:[0],
@@ -178,7 +182,7 @@ function handleSelection(id){
                             text: washFreq,
                             hoverinfo: 'text+name'
                         }
-                        
+        // data for drawing the gauge (an half of a pie chart)
         var pieData = { values: [1,1,1,1,1,1,1,1,1,9],
                             rotation: 90,
                             text: ['8-9', '7-8', '6-7','5-6', '4-5', '3-4', '2-3', '1-2', '0-1'],
@@ -203,16 +207,16 @@ function handleSelection(id){
 
         var layout = {
             autosize:true,
+            // draw needle
             shapes:[{
                 type: 'path',
-                path: gaugePointer(washFreq),
+                path: gaugeNeedle(washFreq),
                 fillcolor: '850000',
                 line: {
                     color: '850000'
                 }
                 }],
             title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per week',
-            autosize:true,
             xaxis: {
                 zeroline:false, 
                 showticklabels:false,
